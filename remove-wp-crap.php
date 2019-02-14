d6ab02edd06e1b926e97193b7a8b8cc10848064d
Dominic Lipscombe <dom@sweatboxevents.com>
2019-02-14 12:39:56 +0000

    Update remove-wp-crap.php

--- remove-wp-crap.php
+++ remove-wp-crap.php
@@ -1,94 +1,99 @@
 <?php
-
 /*
 Plugin Name: Remove WP Crap
 Plugin URI: https://github.com/domlip94/remove-wp-crap
-Description: Removes all the crap from Wordpress outputs 
-Version: 0.0.1
+Description: Removes all the crap from Wordpress outputs
+Version: 0.0.3
 Author: Dominic Lipscombe
 Author URI: http://github.com/domlip94
 Text Domain: remove-wp-crap
 Network: true
 */
+class DLRemoveWpCrap {
+        public function disableEmojis()
+        {
+                remove_action('wp_head', 'print_emoji_detection_script', 7);
+                remove_action('admin_print_scripts', 'print_emoji_detection_script');
+                remove_action('wp_print_styles', 'print_emoji_styles');
+                remove_action('admin_print_styles', 'print_emoji_styles');
+                remove_filter('the_content_feed', 'wp_staticize_emoji');
+                remove_filter('comment_text_rss', 'wp_staticize_emoji');
+                remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+                add_filter('tiny_mce_plugins', 'disableEmojisTinymce');
+                add_filter('wp_resource_hints', 'disableEmojisRemoveDnsPrefetch', 10, 2);
+        }
+        public function disableEmojisTinymce($plugins)
+        {
+                if (is_array($plugins)) {
+                        return array_diff($plugins, array(
+                                'wpemoji'
+                        ));
+                } else {
+                        return array();
+                }
+        }
+        public function disableEmojisRemoveDnsPrefetch($urls, $relation_type)
+        {
+                if ('dns-prefetch' == $relation_type) {
+                        $emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/');
+                        $urls = array_diff($urls, array(
+                                $emoji_svg_url
+                        ));
+                }
+                return $urls;
+        }
 
-function disable_emojis()
-{
-    remove_action('wp_head', 'print_emoji_detection_script', 7);
-    remove_action('admin_print_scripts', 'print_emoji_detection_script');
-    remove_action('wp_print_styles', 'print_emoji_styles');
-    remove_action('admin_print_styles', 'print_emoji_styles');
-    remove_filter('the_content_feed', 'wp_staticize_emoji');
-    remove_filter('comment_text_rss', 'wp_staticize_emoji');
-    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
-    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
-    add_filter('wp_resource_hints', 'disable_emojis_remove_dns_prefetch', 10, 2);
-}
+        public function crunchifyRemoveVersion()
+        {
+                return '';
+        }
+        public function crunchifyCleanupQueryString($src)
+        {
+                $parts = explode('?', $src);
+                return $parts[0];
+        }
+        public function fbFilterQuery($query, $error = true)
+        {
+                if (is_search()) {
+                        $query->is_search = false;
+                        $query->query_vars[s] = false;
+                        $query->query[s] = false;
 
-function disable_emojis_tinymce($plugins)
-{
-    if (is_array($plugins)) {
-        return array_diff($plugins, array(
-            'wpemoji'
-        ));
-    } else {
-        return array();
-    }
-}
-function disable_emojis_remove_dns_prefetch($urls, $relation_type)
-{
-    if ('dns-prefetch' == $relation_type) {
-        $emoji_svg_url = apply_filters('emoji_svg_url', 'https://s.w.org/images/core/emoji/2/svg/');
-        $urls          = array_diff($urls, array(
-            $emoji_svg_url
-        ));
-    }
-    return $urls;
-}
-add_filter('wp_headers', function($headers)
-{
-    unset($headers['X-Pingback']);
-    return $headers;
-});
-add_filter('post_comments_feed_link', function()
-{
-    return null;
-});
-function crunchify_remove_version()
-{
-    return '';
-}
+                        // to error
+                        if ($error == true)
+                                $query->is_404 = true;
+                }
+        }
+        public function returnNull()
+        {
+                return null;
+        }
 
-function crunchify_cleanup_query_string($src)
-{
-    $parts = explode('?', $src);
-    return $parts[0];
-}
+        public function unsetPingback()
+        {
+                unset($headers['X-Pingback']);
+                return $headers;
+        }
 
-function fb_filter_query($query, $error = true)
-{
-    
-    if (is_search()) {
-        $query->is_search     = false;
-        $query->query_vars[s] = false;
-        $query->query[s]      = false;
-        
-        // to error
-        if ($error == true)
-            $query->is_404 = true;
-    }
+        public function exec()
+        {
+                add_filter('wp_headers', ['DLRemoveWpCrap', 'unsetPingback']);
+                add_filter('post_comments_feed_link', ['DLRemoveWpCrap', 'returnNull']);
+                add_filter('script_loader_src', ['DLRemoveWpCrap','crunchifyCleanupQueryString'], 15, 1);
+                add_filter('style_loader_src', ['DLRemoveWpCrap','crunchifyCleanupQueryString'], 15, 1);
+                add_filter('xmlrpc_enabled', '__return_false');
+                add_filter('get_search_form', create_function('$a', "return null;"));
+                add_filter('the_generator', ['DLRemoveWpCrap','crunchifyRemoveVersion']);
+                add_action('init', ['DLRemoveWpCrap','disableEmojis']);
+                add_action('parse_query', ['DLRemoveWpCrap','fbFilterQuery']);
+                remove_action('wp_head', 'rest_output_link_wp_head', 10);
+                remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
+                remove_action('template_redirect', 'rest_output_link_header', 11, 0);
+                remove_action('wp_head', 'rsd_link');
+                remove_action('wp_head', 'wlwmanifest_link');
+                remove_action('wp_head', 'wp_shortlink_wp_head');
+                remove_action('wp_head', 'wlwmanifest_link');
+                remove_action('wp_head', 'feed_links', 2);
+        }
 }
-add_filter('script_loader_src', 'crunchify_cleanup_query_string', 15, 1);
-add_filter('style_loader_src', 'crunchify_cleanup_query_string', 15, 1);
-add_filter('xmlrpc_enabled', '__return_false');
-add_action('init', 'disable_emojis');
-add_action('parse_query', 'fb_filter_query');
-add_filter('get_search_form', create_function('$a', "return null;"));
-add_filter('the_generator', 'crunchify_remove_version');
-remove_action('wp_head', 'rest_output_link_wp_head', 10);
-remove_action('wp_head', 'wp_oembed_add_discovery_links', 10);
-remove_action('template_redirect', 'rest_output_link_header', 11, 0);
-remove_action('wp_head', 'rsd_link');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'wp_shortlink_wp_head');
-remove_action('wp_head', 'wlwmanifest_link');
-remove_action('wp_head', 'feed_links', 2);
+DLRemoveWpCrap::exec();
